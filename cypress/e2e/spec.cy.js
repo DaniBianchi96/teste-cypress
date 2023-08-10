8079e0980cc26dcd44817428b87c276dc50a1a14
Daniel <danielbianchi9639@gmail.com>
2023-08-10 13:33:31 -0700

    Cria codigo

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.google.com.br')
+    cy.get('#APjFqb').type('facebook')
+    cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click()
+    cy.get('.dmenKe > .cIkxbf > .usJj9c > h3 > .l').click()
+  })
+})
