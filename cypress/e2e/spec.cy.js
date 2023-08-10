2e6ae4119599914335a8d8a3bf1b0ccd6c4aec18
Daniel <danielbianchi9639@gmail.com>
2023-08-10 15:38:43 -0700

    Mensagem

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.google.com.br')
-    cy.get('#APjFqb').type('facebooka')
+    cy.get('#APjFqb').type('facebook')
     cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click()
     cy.get('.dmenKe > .cIkxbf > .usJj9c > h3 > .l').click()
+    cy.screenshot()
   })
 })
